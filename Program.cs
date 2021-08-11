8b50e37a7dce5c1c8305bb352576064bddf018a2
Mariana Licona <m.licona@abtis.de>
2021-08-11 16:25:41 +0200

    project  ready to debug

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace AbilityScoreCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the ability score calculator");
+            Console.WriteLine("1) I have created a class");
+            Console.WriteLine("2) I instanciated the class");
+            AbilityScoreCalculator MyScoreCalculator = new AbilityScoreCalculator();
         }
     }
 }
